485fe453975edb498eaae2b97db7c006964aa7c1
George Schneeloch <gschneel@mit.edu>
2017-06-12 13:41:39 -0400

    Use no-cache

--- run_cookiecutter_tests.sh
+++ run_cookiecutter_tests.sh
@@ -15,8 +15,8 @@ docker-compose ps
 # Make sure we start with a fresh container
 docker-compose -f travis-docker-compose.yml kill
 docker-compose -f travis-docker-compose.yml rm -f
-#docker-compose -f travis-docker-compose.yml build --no-cache
-docker-compose -f travis-docker-compose.yml build
+docker-compose -f travis-docker-compose.yml build --no-cache
+#docker-compose -f travis-docker-compose.yml build
 
 docker-compose -f travis-docker-compose.yml run web tox
 docker-compose run watch yarn install
