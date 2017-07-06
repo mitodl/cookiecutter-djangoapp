15fc29b24ee34f2046cc76e8bfda91f784646263
George Schneeloch <gschneel@mit.edu>
2017-07-06 12:30:10 -0400

    WIP

--- run_cookiecutter_tests.sh
+++ run_cookiecutter_tests.sh
@@ -4,7 +4,8 @@ set -euf -o pipefail
 export TEMPDIR="$(mktemp -d)"
 cookiecutter . -o "$TEMPDIR" --no-input
 
-export TOXINI_DIR=$(find "$TEMPDIR" -name tox.ini -printf '%h\n')
+export TOXINI_DIR="$(find "$TEMPDIR" -name tox.ini -printf '%h\n')"
+export PROJECT_NAME="$(basename "$TOXINI_DIR")"
 cd "$TOXINI_DIR"
 cp "$TOXINI_DIR"/.env.example "$TOXINI_DIR"/.env
 pip-compile
@@ -19,7 +20,8 @@ docker-compose -f travis-docker-compose.yml build --no-cache
 
 docker-compose -f travis-docker-compose.yml run web tox
 
-docker build --no-cache -t travis-watch -f ./Dockerfile-node .
+docker build --no-cache -f ./travis/Dockerfile-travis-watch-build -t mitodl/${PROJECT_NAME}_watch_travis .
+docker build --no-cache -f ./travis/Dockerfile-travis-watch -t travis-watch .
 ./travis/js_tests.sh
 
 echo "Success!"
