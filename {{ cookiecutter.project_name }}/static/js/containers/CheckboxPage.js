7fd2dba1c4fb3df31ba2284162fdcffc5d24d578
George Schneeloch <gschneel@mit.edu>
2017-08-08 12:33:35 -0400

    Fixed IntegrationTestHelper, upgraded to react-router 4.x (#82)

---
+++ {{ cookiecutter.project_name }}/static/js/containers/CheckboxPage.js
@@ -0,0 +1,42 @@
+// @flow
+import React from 'react';
+import { connect } from 'react-redux';
+import type { Match } from 'react-router';
+import type { Action } from 'redux';
+
+import {
+  updateCheckbox
+} from '../actions';
+
+class CheckboxPage extends React.Component {
+  props: {
+    dispatch: (action: Action) => void,
+    checkbox: {
+      checked: boolean,
+    },
+    match: Match,
+  };
+
+  handleClick(e) {
+    const { dispatch } = this.props;
+    dispatch(updateCheckbox(e.target.checked));
+  }
+
+  render() {
+    const { checked } = this.props.checkbox;
+    return <div>
+      <div id="app-body">
+        Click the checkbox:
+        <input type="checkbox" checked={checked} onClick={this.handleClick.bind(this)} />
+      </div>
+    </div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    checkbox: state.checkbox,
+  };
+};
+
+export default connect(mapStateToProps)(CheckboxPage);
