23e807b64226bad1e033a45d9e935d540156bc9f
George Schneeloch <gschneel@mit.edu>
2017-12-29 17:44:59 -0500

    Have update-docker-hub.sh update references to next image

--- {{ cookiecutter.project_name }}/travis/update-docker-hub.sh
+++ {{ cookiecutter.project_name }}/travis/update-docker-hub.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -eo pipefail
 
-docker build -t mitodl/{{ cookiecutter.project_name }}_web_travis_next -f Dockerfile .
-docker build -t mitodl/{{ cookiecutter.project_name }}_watch_travis -f travis/Dockerfile-travis-watch-build .
+export NEXT=$(date | md5sum | cut -c -6)
+export PROJECT_NAME="{{ cookiecutter.project_name }}"
+echo "Next hash is $NEXT"
 
-docker push mitodl/{{ cookiecutter.project_name }}_web_travis_next
-docker push mitodl/{{ cookiecutter.project_name }}_watch_travis
+export WEB_IMAGE=mitodl/${PROJECT_NAME}_web_travis_${NEXT}
+export WATCH_IMAGE=mitodl/${PROJECT_NAME}_watch_travis_${NEXT}
+
+echo docker build -t $WEB_IMAGE -f Dockerfile .
+echo docker build -t $WATCH_IMAGE -f travis/Dockerfile-travis-watch-build .
+
+echo docker push $WEB_IMAGE
+echo docker push $WATCH_IMAGE
+
+sed -i "s/^FROM mitodl\/.+$/$WEB_IMAGE/" travis/Dockerfile-travis-web
+sed -i "s/^FROM mitodl\/.+$/$WATCH_IMAGE/" travis/Dockerfile-travis-watch
