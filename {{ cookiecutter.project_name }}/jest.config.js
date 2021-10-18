f6af252c74a1c2459ce50fd52a7cc4049c1f5a29
George Schneeloch <gschneel@mit.edu>
2021-10-18 14:10:01 -0400

    USe ts-jest for transforming all javascript files

--- {{ cookiecutter.project_name }}/jest.config.js
+++ {{ cookiecutter.project_name }}/jest.config.js
@@ -6,8 +6,7 @@ module.exports = {
   ],
   cacheDirectory: ".jest-cache",
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.js$": "babel-jest",
+    "^.+\\.[tj]sx?$": "ts-jest"
   },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
